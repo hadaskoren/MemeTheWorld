275b634e4e94a240d7e6b06d41cda651a816e9d9
hadaskoren <das.koren@gmail.com>
2016-12-06 15:35:06 +0200

    nav bar edits

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ function renderImages() {
     // elMemesGallery.appendChild(elClonedImageTemplate);
     
 }
+
+//------TODO: This doesn't work except for the log'
+function memeEditor() {
+    console.log('EDITOR!');
+    var editor = document.querySelector('.gallery-editor');
+    var gallery = document.querySelector('.gallery');
+    gallery.display = 'none';
+    editor.display = 'block';
+}
