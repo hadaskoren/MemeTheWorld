7ae0fadfecc624c7a72605c2fe37037e25a29ccd
hadaskoren <das.koren@gmail.com>
2016-12-06 14:34:44 +0200

    HTML template is done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 'use strict';
 console.log('Hello Meme');
+
+'use strict';
+
+function renderImages() {
+    // // Create a clone from the template
+    // var elClonedImageTemplate = document.querySelector('.imageTemplate').cloneNode(true);
+  
+    // var elMemesGallery = document.querySelector('.memes-gallery');
+    // elMemesGallery.appendChild(elClonedImageTemplate);
+    
+}
